9c033b0708024d5e05fe23dc5deaf1c97fb58d79
JOSE <andresjose423@gmail.com>
2025-03-27 07:42:27 -0500

    topmate

--- constants.ts
+++ constants.ts
@@ -46,6 +46,7 @@ export const SOCIAL_LINKS = {
   linkedin: "https://www.linkedin.com/in/joseandressandovaljimenez/",
   instagram: "https://www.instagram.com/josesandtech/",
   twitter: "https://twitter.com/andresjose_423",
+  topmate: "https://www.linkedin.com/in/joseandressandovaljimenez/"
 };
 
 export interface IProject {
